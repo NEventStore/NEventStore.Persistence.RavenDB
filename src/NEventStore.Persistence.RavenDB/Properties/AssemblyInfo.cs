2d7fe4ae243eafb0739e75d1b21420fed43bea4b
Damian Hickey <dhickey@gmail.com>
2014-03-03 21:32:56 +0100

    Import RavenDB project files

---
+++ src/NEventStore.Persistence.RavenDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NEventStore.Persistence.RavenPersistence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NEventStore")]
+[assembly: AssemblyCopyright("Copyright © NEventStore Dev Team 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dce4302a-faf0-4b9d-a543-5389b7c44935")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
