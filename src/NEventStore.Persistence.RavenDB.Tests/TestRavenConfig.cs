dfca7e5d42048b203884c68fd48ace117a86357d
Alessandro Colla <eglathon@gmail.com>
2014-07-09 12:33:52 +0200

    Solution compile. Now I can start fixing tests and test the new functionalities

--- src/NEventStore.Persistence.RavenDB.Tests/TestRavenConfig.cs
+++ src/NEventStore.Persistence.RavenDB.Tests/TestRavenConfig.cs
@@ -1,24 +1,32 @@
 namespace NEventStore.Persistence.RavenDB.Tests
 {
-    using System;
-    using System.Transactions;
-    using NEventStore.Persistence.AcceptanceTests;
-    using NEventStore.Persistence.RavenDB;
-    using NEventStore.Serialization;
+  using System;
+  using System.Transactions;
+  using NEventStore.Persistence.AcceptanceTests;
+  using NEventStore.Serialization;
+  using Raven.Database.Config;
 
-    public static class TestRavenConfig
-    {
-        public static RavenConfiguration GetDefaultConfig()
-        {
-            return new RavenConfiguration
-            {
-                Serializer = new DocumentObjectSerializer(),
-                ScopeOption = TransactionScopeOption.Suppress,
-                ConsistentQueries = true, // helps tests pass consistently
-                RequestedPageSize = Int32.Parse("pageSize".GetSetting() ?? "10"), // smaller values help bring out bugs
-                MaxServerPageSize = Int32.Parse("serverPageSize".GetSetting() ?? "1024"), // raven default
-                ConnectionName = "Raven"
-            };
-        }
-    }
+  public static class TestRavenConfig
+  {
+    //public static RavenConfiguration GetDefaultConfig()
+    //{
+    //  return new RavenConfiguration
+    //  {
+    //    Serializer = new DocumentObjectSerializer(),
+    //    ScopeOption = TransactionScopeOption.Suppress,
+    //    ConsistentQueries = true, // helps tests pass consistently
+    //    RequestedPageSize = Int32.Parse("pageSize".GetSetting() ?? "10"), // smaller values help bring out bugs
+    //    MaxServerPageSize = Int32.Parse("serverPageSize".GetSetting() ?? "1024"), // raven default
+    //    ConnectionName = "Raven"
+    //  };
+    //}
+
+
+    public static DocumentObjectSerializer Serializer = new DocumentObjectSerializer();
+    public static TransactionScopeOption ScopeOption = TransactionScopeOption.Suppress;
+    public static bool ConsistentQueries = true; // helps tests pass consistently
+    public static int PageSize = 10; // smaller values help bring out bugs
+    public static string ConnectionName = "Raven";
+
+  }
 }
